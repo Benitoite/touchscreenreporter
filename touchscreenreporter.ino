e3521732267f4231db0c300c9dd0b782ea27c00c
Benitoite <kd6kxr@gmail.com>
2017-12-16 13:42:42 -0800

    Create touchscreenreporter.ino
    To Do: draw X Y and Z on left side.

---
+++ touchscreenreporter.ino
@@ -0,0 +1,62 @@
+#include <stdint.h>
+#include <TouchScreen.h> 
+#include <TFT.h>
+
+#ifdef SEEEDUINO
+#define YP A2   // must be an analog pin, use "An" notation!
+#define XM A1   // must be an analog pin, use "An" notation!
+#define YM 14   // can be a digital pin, this is A0
+#define XP 17   // can be a digital pin, this is A3 
+#endif
+
+#ifdef MEGA
+#define YP A2   // must be an analog pin, use "An" notation!
+#define XM A1   // must be an analog pin, use "An" notation!
+#define YM 54   // can be a digital pin, this is A0
+#define XP 57   // can be a digital pin, this is A3 
+#endif 
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+char xbuff[7];
+char ybuff[7];
+char zbuff[7];
+String xval="0";
+String yval="0";
+String zval="0";
+
+Point p;
+
+void setup()
+{
+
+  Tft.init();  //init TFT library
+  Tft.setDisplayDirect(UP2DOWN);
+  Tft.drawString("TOUCHSCREENREPORTER",12,12,1,CYAN);
+
+}
+
+void loop()
+{
+  p = ts.getPoint();
+
+  xval = String(p.x);
+  xval.toCharArray(xbuff,7);
+  Tft.drawString(xbuff,60,50,5,YELLOW);
+
+
+  yval = String(p.y);
+  yval.toCharArray(ybuff,7);
+  Tft.drawString(ybuff,120,50,5,RED);
+
+
+  zval = String(p.z);
+  zval.toCharArray(zbuff,7);
+  Tft.drawString(zbuff,180,50,5,GREEN);
+  delay(250);
+
+  Tft.fillRectangle(15,48,200,150,BLACK);
+
+
+
+}
+
